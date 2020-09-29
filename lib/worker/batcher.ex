76911240f8b3a45a2e9011e0dd9fac113a26bebb
dvic <info@dvic.io>
2020-09-29 17:09:52 +0200

    use always github user login as name

--- lib/worker/batcher.ex
+++ lib/worker/batcher.ex
@@ -537,7 +537,7 @@ defmodule BorsNG.Worker.Batcher do
                       tree: merge_commit.tree,
                       parents: [prev_head],
                       commit_message: commit_message,
-                      committer: %{name: user.name || user.login, email: user_email}
+                      committer: %{name: user.login, email: user_email}
                     }
                   )
 
